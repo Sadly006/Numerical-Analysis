6c7b35b0264dddaad09a8f7c1a55dcd1cb8b9862
Sadly006 <43539976+Sadly006@users.noreply.github.com>
2019-10-30 05:57:08 +0600

    Create bisection_method.m

---
+++ bisection_method.m
@@ -0,0 +1,15 @@
+f = @(x) (3*x - cos(x) - 1);
+a=input("a : ");
+b=input("b : ");
+for i=1:100
+  c = (a+b)*0.5;
+  m = f(a);
+  n = f(b);
+  o = f(c);
+  fprintf("a = %f b = %f c = %f f(a) = %f f(b) = %f f(c) = %f\n", a, b, c, m, n, o)
+  if m*o < 0
+    b = c;
+  else
+    a = c;
+  endif
+end
